f41258089ea87288a5d4d3594eb9cd759be520d1
unknown <victor@magiros.com.au>
2020-06-07 15:52:09 +1000

    code i have so far, has a few interactions with ebay set up

---
+++ src/test/java/base/basetests.java
@@ -0,0 +1,29 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class basetests {
+    private WebDriver driver;
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+        driver.get("https://www.ebay.com.au/");
+        WebElement searchbar = driver.findElement(By.id("gh-ac"));
+        searchbar.click();
+        searchbar.sendKeys("asdf");
+        WebElement search = driver.findElement(By.id("gh-btn"));
+        search.submit();
+
+
+        System.out.println(driver.getTitle());
+        //driver.quit();
+    }
+public static void main(String args[]){
+        basetests test = new basetests();
+        test.setUp();
+    }
+}
