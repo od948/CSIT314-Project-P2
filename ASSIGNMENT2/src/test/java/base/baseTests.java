432b1cab873cfeb9250db499770b4e9ee56a787f
unknown <victor@magiros.com.au>
2020-06-08 18:26:17 +1000

    add to cart and delete from cart working

--- ASSIGNMENT2/src/test/java/base/baseTests.java
+++ ASSIGNMENT2/src/test/java/base/baseTests.java
@@ -4,26 +4,62 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class baseTests {
     private WebDriver driver;
-    public void setUp(){
+    private WebDriver tempdriver;
+    public void setUp() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        //driver.manage().window().maximize();
+
+        ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200", "--ingore-certificate-errors");
+        driver = new ChromeDriver(options);
+
         driver.get("https://www.ebay.com.au/");
-        WebElement searchbar = driver.findElement(By.id("gh-ac"));
-        searchbar.click();
-        searchbar.sendKeys("asdf");
-        WebElement search = driver.findElement(By.id("gh-btn"));
-        search.submit();
+        System.out.println("open ebay \n");
+
+        driver.findElement(By.id("gh-ac")).click();
+        driver.findElement(By.id("gh-ac")).sendKeys("asdf");
+        driver.findElement(By.id("gh-btn")).submit();
+        System.out.println("click search bar, enter 'asdf' and submit \n");
 
 
-        System.out.println(driver.getTitle());
-        //driver.quit();
+        for(String winHandle : driver.getWindowHandles()){
+            driver.switchTo().window(winHandle);
+        }
+
+        System.out.println("current page: " + driver.getTitle());
+
+        driver.quit();
+
+        tempdriver = new ChromeDriver(options);
+
+
+        tempdriver.get("https://www.ebay.com.au/itm/3X-Lightning-Data-Charger-Cable-Cord-Compatible-for-Apple-iPhone-5-6-S-7-8-X/153854545450?hash=item23d2720e2a:g:Qx8AAOSwnhFeYQ55");
+        System.out.println("current page: " + tempdriver.getTitle());
+        tempdriver.findElement(By.id("atcRedesignId_btn")).click();
+        System.out.println("item added to cart");
+        Thread.sleep(500);
+        tempdriver.get("https://cart.payments.ebay.com.au/sc/view");
+        System.out.println("cart opened");
+        Thread.sleep(500);
+        System.out.println("cart size = " + tempdriver.findElements(By.className("cart-bucket-lineitem")).size());
+        Boolean isPresent = tempdriver.findElements(By.className("cart-bucket-lineitem")).size() > 0;
+        while(isPresent){
+
+            tempdriver.findElement(By.cssSelector("button[data-test-id='cart-remove-item']")).click();
+            System.out.println("deleted first item" + "\n");
+            if(tempdriver.findElements(By.className("cart-bucket-lineitem")).size() > 0) isPresent=false;
+            Thread.sleep(500);
+            System.out.println("cart size = " + tempdriver.findElements(By.className("cart-bucket-lineitem")).size());
+        }
+        tempdriver.quit();
     }
-    public static void main(String args[]){
+    public static void main(String args[]) throws InterruptedException {
         baseTests test = new baseTests();
         test.setUp();
     }
